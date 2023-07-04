3dcf415514955dee7c8140a79e7753dba5748ee8
Ahmet Tunahan Bekdaş <ahmettunahanbekdas@gmail.com>
2023-07-04 16:45:58 +0300

    Added Segue

--- BMI Calculator/Controllers/CalculateViewController.swift
+++ BMI Calculator/Controllers/CalculateViewController.swift
@@ -1,15 +1,9 @@
-//
-//  ViewController.swift
-//  BMI Calculator
-//
-//  Created by Angela Yu on 21/08/2019.
-//  Copyright © 2019 Angela Yu. All rights reserved.
-//
 
 import UIKit
 
-class  CalculateViewController: UIViewController {
+class CalculateViewController: UIViewController {
     
+    var bmiValue = "0.0"
     
     @IBOutlet weak var heightLabel: UILabel!
     @IBOutlet weak var weightLabel: UILabel!
@@ -19,9 +13,7 @@ class  CalculateViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-    
     
     @IBAction func heightValue(_ sender: UISlider) {
         let heightValue = sender.value
@@ -35,16 +27,24 @@ class  CalculateViewController: UIViewController {
         weightLabel.text = "\(doubleWeight)Kg"
     }
     
-    
-    @IBAction func buttonPressed(_ sender: Any) {
+    @IBAction func buttonPressed(_ sender: UIButton) {
         let height = heightSlider.value
         let weight = Double(weightSlider.value)
         
-        let bmı = weight / pow(Double(height), 2)
+        let bmi = weight / pow(Double(height), 2)
+        bmiValue = String(format: "%.1f", bmi)
         
-        self.performSegue(withIdentifier:"goToResult", sender: self)
+        self.performSegue(withIdentifier: "goToResult", sender: self)
+
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goToResult" {
+            let destinationVC = segue.destination as! ResultViewController
+            destinationVC.bmiValue = bmiValue
+            
+        }
+    }
     
 }
 
